4b9c6b03723c055aafc088c16476a92d5832ecc1
tunglain2 <tunglain2@gmail.com>
2018-12-19 14:22:14 +0800

    java and kotlon variable

--- src/main/java/com/tunglain/Hello.java
+++ src/main/java/com/tunglain/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello World");
       Person p = new Person();
       p.hello();
+      int age = 19;
+      Integer age2 = 19;
+      char a = 'A';
+      Character c = 'B';
+      byte b = 120;
+      float weight = 66;
+      float height = 1.7f;
+      boolean adult = true;
+      boolean enroll = false;
+      String name = "steven";
     }
 }
--- src/main/kotlin/com/tunglain/Hello.kt
+++ src/main/kotlin/com/tunglain/Hello.kt
@@ -5,6 +5,13 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+    val age : Int = 19
+    var age2 = 20
+    age2 = 23
+    var weight = 66.5f
+    val name  = "steven"
+    val name2 : String
+    name2 = "peter"
 }
 
 class Human {
